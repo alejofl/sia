af1a026cd18898d6056ce06cf36c2a6f72b65e4d
Alejo Flores Lucey <alejo@misterflores.com>
2024-08-19 11:25:30 -0300

    Change the way filenames are handled to avoid problems

--- tp1/main.py
+++ tp1/main.py
@@ -1,5 +1,6 @@
 import json
 import sys
+import time
 from src.classes.parser import Parser
 from src.classes.printer import Printer
 from src.classes.manager import SokobanManager
@@ -8,18 +9,20 @@ from src.classes.manager import SokobanManager
 if __name__ == "__main__":
     with open(sys.argv[1], "r") as configFile:
         config = json.load(configFile)
+        filenameTemplate = f"{str(int(time.time()))}_{config['algorithm'].replace('*', 'STAR')}{'_' + config['heuristic'] if config['algorithm'] in ('GREEDY', 'A*') else ''}_%s"
+
         board, player, boxes, goals = Parser(config["board"]).parse()
         
         sm = SokobanManager(board, goals, player, boxes)
-        print(f'Running {config["algorithm"]} algorithm')
+        print(f"Running {config['algorithm']} algorithm")
         
         benchmark = sm.run(config["algorithm"], config["heuristic"])
         print("Winning path found!")
         print(benchmark)
-        
+
         if config["animation"] == "GIF":
-            Printer.gif(config["animationFile"])
+            Printer.gif(filenameTemplate % ("animation.gif"))
         elif config["animation"] == "ASCII":
-            Printer.ascii(config["animationFile"])
+            Printer.ascii(filenameTemplate % ("animation.txt"))
 
     sys.exit(0)
