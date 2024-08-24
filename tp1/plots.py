755cb63ef9a4c3590be33982c133bea967aa26b5
Andres Carro <acarro@itba.edu.ar>
2024-08-24 19:29:13 -0300

    Add pip dependencies, remove prints and fix csv flies paths

--- tp1/plots.py
+++ tp1/plots.py
@@ -5,23 +5,43 @@ import glob
 import matplotlib.pyplot as plt
 
 def plot_all_algs(level, heur, variable):
-    csv_files_no_heur = glob.glob(f'results/*_*_{level}_results.csv')
+    csv_files_no_heur = glob.glob(f'results/{level}/*_*_{level}_results.csv')
     csv_files_heur = glob.glob(f'results/*_*_{heur}_{level}_results.csv')
     csv_files = csv_files_no_heur + csv_files_heur
 
-    print(csv_files)
     result_df = pd.DataFrame()
     for file in csv_files:
         df = pd.read_csv(file)
         alg_name = file.split("_")[1]
-        cost = df[variable][0] if variable != "meanExecutionTime" else df["executionTime"].mean() 
+        cost = df[variable][0] if variable != "meanExecutionTime" else df["executionTime"].mean()
         new_row = {'algorithm': alg_name, variable: cost}
         result_df = pd.concat([result_df, pd.DataFrame([new_row])], ignore_index=True)
-    print(result_df)
+
     plt.figure(figsize=(8, 6))
     plt.bar(result_df['algorithm'], result_df[variable], color=['blue', 'green'])
 
-    plt.title(variable+' of Algorithms')
+    plt.title(variable + ' of Algorithms')
+    plt.xlabel('Algorithm')
+    plt.ylabel(variable)
+
+    plt.show()
+
+
+def plot_bfs_vs_dfs(level,variable):
+    csv_files = glob.glob(f'results/{level}/*_*_{level}_results.csv')
+
+    result_df = pd.DataFrame()
+    for file in csv_files:
+        df = pd.read_csv(file)
+        alg_name = file.split("_")[1]
+        cost = df[variable][0] if variable != "meanExecutionTime" else df["executionTime"].mean()
+        new_row = {'algorithm': alg_name, variable: cost}
+        result_df = pd.concat([result_df, pd.DataFrame([new_row])], ignore_index=True)
+
+    plt.figure(figsize=(8, 6))
+    plt.bar(result_df['algorithm'], result_df[variable], color=['blue', 'green'])
+
+    plt.title(variable + ' of Algorithms')
     plt.xlabel('Algorithm')
     plt.ylabel(variable)
 
@@ -30,4 +50,3 @@ def plot_all_algs(level, heur, variable):
 if __name__ == "__main__":
     print("main is running!")
     plot_all_algs("soko01", "MANHATTAN", "meanExecutionTime")
-
