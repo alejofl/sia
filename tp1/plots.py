db75f680da20a275a0a9368fc2ebcd74cec41dc2
Andres Carro <acarro@itba.edu.ar>
2024-08-24 21:34:41 -0300

    Add bfs and dfs execution time comparison between different levels

--- tp1/plots.py
+++ tp1/plots.py
@@ -81,7 +81,44 @@ def plot_bfs_vs_dfs_costs():
 
     plt.xlabel('Algorithms')
     plt.ylabel('Amount of steps till completion')
-    plt.title('Cost of algorithms in different levels')
+    plt.title('Cost of Algorithms in Different Levels')
+    plt.xticks(index + width / 2, levels)
+    plt.legend()
+
+    plt.show()
+
+
+def plot_bfs_vs_dfs_exectime():
+    bfs_csvs = []
+    dfs_csvs = []
+    bfs_exectime = []
+    dfs_exectime = []
+    levels = ['soko01','soko02','soko03','soko04','soko05','soko06']
+    for i in range(1,7):
+        bfs_csvs += glob.glob(f'results/soko0{i}/*_BFS_soko0{i}_results.csv')
+        dfs_csvs += glob.glob(f'results/soko0{i}/*_DFS_soko0{i}_results.csv')
+
+    for file in bfs_csvs + dfs_csvs:
+        df = pd.read_csv(file)
+        alg_name = file.split("_")[1]
+        match alg_name:
+            case 'BFS':
+                print(df["executionTime"].mean())
+                bfs_exectime.append(df["executionTime"].mean())
+            case 'DFS':
+                dfs_exectime.append(df["executionTime"].mean())
+
+    index = np.arange(len(levels))
+    width = 0.35  # ancho de las barras
+
+    plt.figure(figsize=(8, 6))
+
+    plt.bar(index, bfs_exectime, width=width, label='BFS')
+    plt.bar(index + width, dfs_exectime, width=width, label='DFS')
+
+    plt.xlabel('Algorithms')
+    plt.ylabel('Time in seconds')
+    plt.title('Execution Time of Algorithms in Different Levels')
     plt.xticks(index + width / 2, levels)
     plt.legend()
 
@@ -92,4 +129,5 @@ if __name__ == "__main__":
     print("Running plots")
     #plot_all_algs("soko01", "MANHATTAN", "meanExecutionTime")
     #plot_bfs_vs_dfs_cost("soko02") #soko02 is dummy level
-    plot_bfs_vs_dfs_costs() # graph that shows cost per level per alg
+    #plot_bfs_vs_dfs_costs() # graph that shows cost per level per alg
+    plot_bfs_vs_dfs_exectime()
