e226e507cd20c0cb47dce669f2e0c1c97b605424
Alejo Flores Lucey <alejo@misterflores.com>
2024-09-06 18:12:25 -0300

    Implement recognision of config file in EVE run method
    Co-authored-by: Andres Carro Wetzel <acarro@itba.edu.ar>
    Co-authored-by: ian <itognetti@itba.edu.ar>

--- tp2/src/classes/eve.py
+++ tp2/src/classes/eve.py
@@ -1,11 +1,11 @@
+import time
 import numpy as np
 from .player import Player
 
 
-class EVE:
-    def __init__(self, selectionMethods, ):
-        # TODO
-        pass
+class EVE:    
+    def __init__(self, config):
+        self.config = config
 
     @staticmethod
     def selectElite(population, k, options = None):
@@ -125,15 +125,201 @@ class EVE:
                 newPopulation.append(player1)
 
         return newPopulation
- 
-    # poblacion = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
-    # padres = [1, 2, 3, 4, 5, 6, 7]
-    #                 |
-    # hijos = [1-2, 2-7, 3-1, ...]
-    def run(self, initialPopulation, playerClass, totalPoints, maxTime):
-        population = [Player.generateRandomPlayer(playerClass, totalPoints) for _ in range(initialPopulation)]
+    
+    @staticmethod
+    def onePointCrossover(player1, player2, options = None):
+        gen = np.random.default_rng()
+
+        player1Alleles = player1.getalleles()
+        player2Alleles = player2.getalleles()
+
+        randomIndex = gen.integers(0, len(player1Alleles))
+
+        return player1.__class__(player1.totalPoints, *player1Alleles[:randomIndex], *player2Alleles[randomIndex:]), player1.__class__(player1.totalPoints, *player2Alleles[:randomIndex], *player1Alleles[randomIndex:])
+
+    @staticmethod
+    def twoPointsCrossover(player1, player2, options = None):
+        gen = np.random.default_rng()
+
+        player1Alleles = player1.getalleles()
+        player2Alleles = player2.getalleles()
+
+        firstIndex, secondIndex = gen.integers(0, len(player1Alleles), 2)
+
+        if firstIndex > secondIndex:
+            firstIndex, secondIndex = secondIndex, firstIndex
+
+        return player1.__class__(player1.totalPoints, *player1Alleles[:firstIndex], *player2Alleles[firstIndex:secondIndex], *player1Alleles[secondIndex:]), player1.__class__(player1.totalPoints, *player2Alleles[:firstIndex], *player1Alleles[firstIndex:secondIndex], *player2Alleles[secondIndex:])
+
+    @staticmethod
+    def anularCrossover(player1, player2, options = None):
+        gen = np.random.default_rng()
+
+        player1Alleles = player1.getalleles()
+        player2Alleles = player2.getalleles()
+        n = len(player1Alleles)
+
+        index, length = gen.integers(0, n, 2)
+
+        child1 = player1Alleles.copy()
+        child2 = player2Alleles.copy()
+        for i in range(length):
+            child1[(index + i) % n] = player2Alleles[(index + i) % n]
+            child2[(index + i) % n] = player1Alleles[(index + i) % n]
+
+        return player1.__class__(player1.totalPoints, *child1), player1.__class__(player1.totalPoints, *child2)
+
+    @staticmethod
+    def uniformCrossover(player1, player2, options = None):
+        gen = np.random.default_rng()
+
+        player1Alleles = player1.getalleles()
+        player2Alleles = player2.getalleles()
+        p = options["p"]
+
+        child1 = []
+        child2 = []
+
+        for a1, a2 in zip(player1Alleles, player2Alleles):
+            rand = gen.random()
+            if rand > p:
+                child1.append(a1)
+                child2.append(a2)
+            else:
+                child1.append(a2)
+                child2.append(a1)
+
+        return player1.__class__(player1.totalPoints, *child1), player1.__class__(player1.totalPoints, *child2)
+
+    def evaluateMaxGenerationsStopCondition(self, population, options):
+        return self.generationCount >= options["maxGenerations"]
+
+    def evaluateStructureStopCondition(self, population, options):
+        if self.previousPopulation is None:
+            return False
+
+        isPersistent = len(np.intersect1d(self.previousPopulation, population)) / len(population) >= options["percentage"]
+        if isPersistent:
+            self.persistentStructureCount += 1
+        else:
+            self.persistentStructureCount = 0
+
+        return self.persistentStructureCount >= options["minRepeatedGenerations"]
+
+    def evaluateContentStopCondition(self, population, options):
+        if self.bestIndividual is None:
+            return False
+
+        bestIndividual = max(population, key=lambda x: x.getFitness())
+        isPersistent = bestIndividual == self.bestIndividual
+        if isPersistent:
+            self.persistentContentCount += 1
+        else:
+            self.persistentContentCount = 0
+
+        return self.persistentContentCount >= options["minRepeatedGenerations"]
+
+    def evaluateDeltaStopCondition(self, population, options):
+        if self.bestIndividual is None:
+            return False
+
+        bestIndividual = max(population, key=lambda x: x.getFitness())
+        delta = abs(bestIndividual.getFitness() - self.bestIndividual.getFitness())
+        isPersistent = delta <= options["delta"]
+        if isPersistent:
+            self.persistentDeltaCount += 1
+        else:
+            self.persistentDeltaCount = 0
+
+        return self.persistentDeltaCount >= options["minRepeatedGenerations"]
+
+    SELECTION_FUNCTIONS = {
+        "ELITE": selectElite,
+        "ROULETTE": selectRoulette,
+        "UNIVERSAL": selectUniversal,
+        "BOLTZMANN": selectBoltzmann,
+        "RANKING": selectRanking,
+        "DETERMINISTIC_TOURNAMENT": selectDeterministicTournament,
+        "PROBABILISTIC_TOURNAMENT": selectProbabilisticTournament
+    }
+
+    CROSSOVER_FUNCTIONS = {
+        "ONE_POINT": onePointCrossover,
+        "TWO_POINTS": twoPointsCrossover,
+        "ANULAR": anularCrossover,
+        "UNIFORM": uniformCrossover
+    }
+    
+    def startExecution(self, maxTime):
+        self.startTime = time.time()
+        self.maxTime = maxTime
+        self.generationCount = 1
+        self.previousPopulation = None
+        self.persistentStructureCount = 0
+        self.bestIndividual = None
+        self.persistentContentCount = 0
+        self.persistentDeltaCount = 0
+
+    def evaluateStopCondition(self, population):
+        if time.time() - self.startTime >= self.maxTime:
+            return True
+
+        stopCondition = False
+        for stopCondition in self.config["stopCondition"]:
+            match stopCondition["method"]:
+                case "MAX_GENERATIONS":
+                    stopCondition = stopCondition and self.evaluateMaxGenerationsStopCondition(population, stopCondition["options"])
+                case "STRUCTURE":
+                    stopCondition = stopCondition and self.evaluateStructureStopCondition(population, stopCondition["options"])
+                case "CONTENT":
+                    stopCondition = stopCondition and self.evaluateContentStopCondition(population, stopCondition["options"])
+                case "DELTA":
+                    stopCondition = stopCondition and self.evaluateDeltaStopCondition(population, stopCondition["options"])
+
+        self.previousPopulation = population
+        self.bestIndividual = max(population, key=lambda x: x.getFitness())
+        self.generationCount += 1
+        return stopCondition
+
+    def run(self, playerClass, totalPoints, maxTime):
+        population = [Player.generateRandomPlayer(playerClass, totalPoints) for _ in range(self.config["populationSize"])]
         
-        # Select parents
-        # Generate children, with crossover and mutation
-        # Pick new population
-        # Repeat
+        keepRunning = True
+        while keepRunning:
+            parents = []
+            for selectionMethod in self.config["selection"]:
+                matesForSelection = np.ceil(self.config["matesPerGeneration"] * selectionMethod["percentage"])
+                parents.extend(self.SELECTION_FUNCTIONS[selectionMethod["method"]](population, matesForSelection, selectionMethod["options"]))
+
+            children = []
+            parentsCount = len(parents)
+            i = 0
+            while i < parentsCount:
+                parent1 = parents[i]
+                parent2 = parents[i + 1 if i + 1 < parentsCount else i]
+                children.extend(self.CROSSOVER_FUNCTIONS[self.config["crossover"]["method"]](parent1, parent2, self.config["crossover"]["options"]))
+                # TODO: Mutation
+                i += 2
+
+            newPopulation = []
+            if self.config["pick"]["type"] == "YOUNG_BIASED":
+                populationPlacesLeft = self.config["populationSize"]
+                if len(children) <= self.config["populationSize"]:
+                    newPopulation.extend(children)
+                    populationPlacesLeft -= len(children)
+                    if populationPlacesLeft > 0:
+                        for pickMethod in self.config["pick"]["selectionMethods"]:
+                            qty = np.ceil(populationPlacesLeft * pickMethod["percentage"])
+                            newPopulation.extend(self.SELECTION_FUNCTIONS[pickMethod["method"]](population, qty, pickMethod["options"]))
+                else:
+                    for pickMethod in self.config["pick"]["selectionMethods"]:
+                        qty = np.ceil(self.config["populationSize"] * pickMethod["percentage"])
+                        newPopulation.extend(self.SELECTION_FUNCTIONS[pickMethod["method"]](children, qty, pickMethod["options"]))
+            else:
+                people = population + children
+                for pickMethod in self.config["pick"]["selectionMethods"]:
+                    qty = np.ceil(self.config["populationSize"] * pickMethod["percentage"])
+                    newPopulation.extend(self.SELECTION_FUNCTIONS[pickMethod["method"]](people, qty, pickMethod["options"]))            
+
+            population = newPopulation[:self.config["populationSize"]]
+            keepRunning = not self.evaluateStopCondition(population)
--- tp2/src/classes/player.py
+++ tp2/src/classes/player.py
@@ -3,13 +3,16 @@ import numpy as np
 
 
 class Player(ABC):
-    def __init__(self, height, strength, skill, intelligence, courage, physique):
+    def __init__(self, totalPoints, height, strength, skill, intelligence, courage, physique):
+        self.totalPoints = totalPoints
         self.height = height
-        self.strength = strength
-        self.skill = skill
-        self.intelligence = intelligence
-        self.courage = courage
-        self.physique = physique
+
+        currentPoints = strength + skill + intelligence + courage + physique
+        self.strength = strength * totalPoints / currentPoints
+        self.skill = skill * totalPoints / currentPoints
+        self.intelligence = intelligence * totalPoints / currentPoints
+        self.courage = courage * totalPoints / currentPoints
+        self.physique = physique * totalPoints / currentPoints
         
     def generateRandomPlayer(playerClass, totalPoints):
         PLAYER_CLASSES = {
@@ -24,71 +27,10 @@ class Player(ABC):
         attributes = r.random(5)
         attributes = attributes / np.sum(attributes) * totalPoints
 
-        return PLAYER_CLASSES[playerClass](height, *attributes)
+        return PLAYER_CLASSES[playerClass](totalPoints, height, *attributes)
 
     def getalleles(self):
         return [self.height, self.strength, self.skill, self.intelligence, self.courage, self.physique]
-
-    def onePointCrossover(self, player2, options = None):
-        gen = np.random.default_rng()
-
-        player1Alleles = self.getalleles()
-        player2Alleles = player2.getalleles()
-
-        randomIndex = gen.integers(0, len(player1Alleles))
-
-        return self.__class__(*player1Alleles[:randomIndex], *player2Alleles[randomIndex:]), self.__class__(*player2Alleles[:randomIndex], *player1Alleles[randomIndex:])
-
-    def twoPointsCrossover(self, player2, options = None):
-        gen = np.random.default_rng()
-
-        player1Alleles = self.getalleles()
-        player2Alleles = player2.getalleles()
-
-        firstIndex, secondIndex = gen.integers(0, len(player1Alleles), 2)
-
-        if firstIndex > secondIndex:
-            firstIndex, secondIndex = secondIndex, firstIndex
-
-        return self.__class__(*player1Alleles[:firstIndex], *player2Alleles[firstIndex:secondIndex], *player1Alleles[secondIndex:]), self.__class__(*player2Alleles[:firstIndex], *player1Alleles[firstIndex:secondIndex], *player2Alleles[secondIndex:])
-
-    def anularCrossover(self, player2, options = None):
-        gen = np.random.default_rng()
-
-        player1Alleles = self.getalleles()
-        player2Alleles = player2.getalleles()
-        n = len(player1Alleles)
-
-        index, length = gen.integers(0, n, 2)
-
-        child1 = player1Alleles.copy()
-        child2 = player2Alleles.copy()
-        for i in range(length):
-            child1[(index + i) % n] = player2Alleles[(index + i) % n]
-            child2[(index + i) % n] = player1Alleles[(index + i) % n]
-
-        return self.__class__(*child1), self.__class__(*child2)
-
-    def uniformCrossover(self, player2, options = None):
-        gen = np.random.default_rng()
-
-        player1Alleles = self.getalleles()
-        player2Alleles = player2.getalleles()
-        p = options["p"]
-
-        child1 = []
-        child2 = []
-
-        for a1, a2 in zip(player1Alleles, player2Alleles):
-            rand = gen.random()
-            if rand > p:
-                child1.append(a1)
-                child2.append(a2)
-            else:
-                child1.append(a2)
-                child2.append(a1)
-
-        return self.__class__(*child1), self.__class__(*child2)
 
     def getNormStrength(self):
         return 100 * np.tanh(0.01 * self.strength)
@@ -116,6 +58,14 @@ class Player(ABC):
     @abstractmethod
     def getFitness(self):
         pass
+
+    def __eq__(self, other):
+        if not isinstance(other, self.__class__):
+            return False
+        return self.height == other.height and self.strength == other.strength and self.skill == other.skill and self.intelligence == other.intelligence and self.courage == other.courage and self.physique == other.physique
+
+    def __hash__(self):
+        return hash((self.height, self.strength, self.skill, self.intelligence, self.courage, self.physique))
 
 
 class Warrior(Player):
