848d556dd2ddadcd0568e5781f44497074724aad
Alejo Flores Lucey <alejo@misterflores.com>
2024-09-26 12:10:43 -0300

    Continue implementation of simple perceptron

--- tp3/src/constants.py
+++ tp3/src/constants.py
@@ -1,14 +1,18 @@
 class Constants:
     _instance = None
 
-    def __new__(cls, beta):
+    def __new__(cls, **kwargs):
         if cls._instance is None:
             cls._instance = super(Constants, cls).__new__(cls)
-            cls._instance._initialize(beta)
+            cls._instance._initialize(**kwargs)
         return cls._instance
 
-    def _initialize(self, beta):
-        self.beta = beta
+    def _initialize(self, **kwargs):
+        self.beta = kwargs.get("beta", 0.1)
+        self.epsilon = kwargs.get("epsilon", 1e-5)
+        self.seed = kwargs.get("seed", None)
+        self.maxEpochs = kwargs.get("maxEpochs", 1000)
+        self.learningRate = kwargs.get("learningRate", 0.1)
 
     @staticmethod
     def getInstance():
--- tp3/src/perceptron.py
+++ tp3/src/perceptron.py
@@ -1,17 +1,22 @@
 from abc import ABC, abstractmethod
+import numpy as np
+from .constants import Constants
 
 
 class Perceptron(ABC):
-    def __init__(self, activationFunction, learningRate, maxEpochs, weights):
+    def __init__(self, activationFunction, weights):
         self.activationFunction = activationFunction
-        self.learningRate = learningRate
-        self.maxEpochs = maxEpochs
         self.weights = weights
-        self.weightCount = len(weights)
-        self.weightsHistory = [weights]
-        
+        self.weightsCount = len(weights)
+        self.weightsHistory = np.array([weights])
+        self.constants = Constants.getInstance()
+
     @abstractmethod
     def train(self, inputs, expectedOutputs):
+        pass
+
+    @abstractmethod
+    def calculateError(self, inputs, expectedOutputs):
         pass
 
     @abstractmethod
@@ -20,7 +25,25 @@ class Perceptron(ABC):
 
 
 class SingleLayerPerceptron(Perceptron):
-    pass
+    def train(self, inputs, expectedOutputs):
+        for _ in range(self.constants.maxEpochs):
+            for input, expectedOutput in zip(inputs, expectedOutputs):
+                output = self.activationFunction(input, self.weights)
+                deltaW = self.constants.learningRate * (expectedOutput - output) * self.activationFunction.derivative(input, self.weights) * input
+                self.weights += deltaW
+                self.weightsHistory.append(np.copy(self.weights))
+
+                if self.calculateError(inputs, expectedOutputs) <= self.constants.epsilon:
+                    return
+
+    def calculateError(self, inputs, expectedOutputs):
+        error = 0
+        for input, expectedOutput in zip(inputs, expectedOutputs):
+            error += np.power(expectedOutput - self.activationFunction(input, self.weights), 2)
+        return error / len(expectedOutputs)
+
+    def test(self, input):
+        return self.activationFunction(input, self.weights)
 
 
 class MultiLayerPerceptron(Perceptron):
