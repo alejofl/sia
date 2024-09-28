f4b4fb2a6996cac8c18cfee71e2564105f4db7b0
Matías Della Torre <matidellatorre@gmail.com>
2024-09-28 19:56:56 -0300

    multilayer [WIP]
    Co-authored-by: Andy <acarro@itba.edu.ar>
    Co-authored-by: Alejo <afloreslucey@itba.edu.ar>
    Co-authored-by: Ian <itognetti@itba.edu.ar>

--- tp3/src/perceptron.py
+++ tp3/src/perceptron.py
@@ -5,13 +5,6 @@ from .function import ActivationFunction
 
 
 class Perceptron(ABC):
-    def __init__(self, activationFunction: ActivationFunction, weights):
-        self.activationFunction = activationFunction
-        self.weights = weights
-        self.weightsCount = len(weights)
-        self.weightsHistory = [weights]
-        self.weightsPerEpoch = []
-        self.constants = Constants.getInstance()
 
     @abstractmethod
     def train(self, inputs, expectedOutputs):
@@ -27,14 +20,22 @@ class Perceptron(ABC):
 
 
 class SingleLayerPerceptron(Perceptron):
+
+    def __init__(self, activationFunction: ActivationFunction, weights):
+        self.activationFunction = activationFunction
+        self.weights = weights
+        self.weightsCount = len(weights)
+        self.weightsHistory = [weights]
+        self.weightsPerEpoch = []
+        self.constants = Constants.getInstance()
+
     def train(self, inputs, expectedOutputs):
         self.activationFunction.configureNormalization(expectedOutputs)
         for _ in range(self.constants.maxEpochs):
             for input, expectedOutput in zip(inputs, expectedOutputs):
                 output = self.activationFunction(input, self.weights)
                 deltaW = self.constants.learningRate * (self.activationFunction.normalize(expectedOutput) - output) * self.activationFunction.derivative(input, self.weights) * input
-                self.weights = np.add(self.weights, deltaW)
-                self.weightsHistory.append(np.copy(self.weights))
+                self.updateWeights(deltaW)
 
                 if np.abs(self.calculateError(inputs, expectedOutputs)) <= self.constants.epsilon:
                     return
@@ -49,7 +50,69 @@ class SingleLayerPerceptron(Perceptron):
 
     def test(self, input):
         return self.activationFunction.denormalize(self.activationFunction(input, self.weights))
+    
+    def updateWeights(self, deltaW):
+        self.weights = np.add(self.weights, deltaW)
+        self.weightsHistory.append(np.copy(self.weights))
 
-
+ # architecture: [ [neuronQty, actFn, [w11, w12, ..], [w21, w22, ...]], .... ]
 class MultiLayerPerceptron(Perceptron):
-    pass
+    def __init__(self, architecture):
+        self.layers = []
+        for qty, activationFn, weights in architecture:
+            layer = []
+            for i in range(qty):
+                n = SingleLayerPerceptron(activationFn, weights[i])
+                layer.append(n)
+            self.layers.append(layer)
+        self.constants = Constants.getInstance()
+
+    def train(self, inputs, expectedOutputs):
+        # TODO: define normalization
+        for _ in range(self.constants.maxEpochs):
+            for input, expectedOutput in zip(inputs, expectedOutputs):
+                outputs = np.array()
+                for i, layer in enumerate(self.layers):
+                    layerInput = outputs[i-1] if i>0 else input
+                    layerOutput = map(lambda n:n.test(layerInput), layer)
+                    outputs = np.append(outputs, [layerOutput])
+
+                deltas = []
+                for i, layer in reversed(list(enumerate(self.layers))):
+                    if i==len(self.layers):
+                        layerDeltas = []
+                        for k, (n, expected) in enumerate(zip(layer, expectedOutput)):
+                            delta = (expected - outputs[i][k])*n.activationFunction.derivative(outputs[i-1], n.weights)
+                            layerDeltas.append(delta)
+                            # TODO: deltaW
+
+                        deltas.append(layerDeltas)
+                    else:
+                        layerDeltas = []
+                        for k, n in enumerate(layer):
+                            delta = np.dot(deltas[-1], self.layers[i+1][k].weights)*n.activationFunction.derivative(outputs[i-1], n.weights)
+                            layerDeltas.append(delta)
+                            # TODO: deltaW
+
+                        deltas.append(layerDeltas)
+                
+
+        
+                # deltaW = 
+                # self.updateWeights(deltaW)
+
+                if np.abs(self.calculateError(inputs, expectedOutputs)) <= self.constants.epsilon:
+                    return
+            self.weightsPerEpoch.append(np.copy(self.weights))
+        print("Training finished without convergence.")
+
+        return 1
+    
+    def calculateError(self, inputs, expectedOutputs):
+        return 1
+
+    def test(self, input):
+        return 1
+        
+
+       
