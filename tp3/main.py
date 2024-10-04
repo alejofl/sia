fix: normalization

tp3/main.py
@@ -9,6 +9,7 @@ from src.optimizer import OptimizerFunction
 from src.perceptron import SingleLayerPerceptron, MultiLayerPerceptron
 from src.utils import Utils
 
+# Exercise 1
 
 AND_DATASET_PATH = os.path.join(os.path.dirname(sys.argv[0]), "resources", "ej1", "and.csv")
 def solveAnd(config):
@@ -27,7 +28,6 @@ def solveAnd(config):
     print(p.test(np.array([1, 1, -1]))) # -1
     print(p.test(np.array([1, 1, 1]))) # 1
 
-
 XOR_DATASET_PATH = os.path.join(os.path.dirname(sys.argv[0]), "resources", "ej1", "xor.csv")
 def solveXor(config):
     dataset = pd.read_csv(XOR_DATASET_PATH)
@@ -45,6 +45,7 @@ def solveXor(config):
     print(p.test(np.array([1, 1, -1]))) # 1
     print(p.test(np.array([1, 1, 1]))) # -1
 
+# Exercise 2
 
 SET_DATASET_PATH = os.path.join(os.path.dirname(sys.argv[0]), "resources", "ej2", "set.csv")
 def solveSet(config):
@@ -67,6 +68,7 @@ def solveSet(config):
         print(output, expectedOutput, np.abs(output - expectedOutput) <= Constants.getInstance().epsilon, sep=" ")
     Utils.mseVsEpoch(p, trainingInputs, trainingExpectedOutputs, testingInputs, testingExpectedOutputs, "setMSE.csv")
 
+# Exercise 3
 
 def solveMultilayerXor(config):
     dataset = pd.read_csv(XOR_DATASET_PATH)

tp3/src/perceptron.py
@@ -20,7 +20,7 @@ class Perceptron(ABC):
 
 
 class SingleLayerPerceptron(Perceptron):
-    def __init__(self, activationFunction: ActivationFunction, optimizer: OptimizerFunction, weights):
+    def __init__(self, activationFunction: ActivationFunction, optimizer: OptimizerFunction, weights, isMultiLayer=False):
         self.activationFunction = activationFunction
         self.optimizer = optimizer
         self.weights = weights
@@ -29,6 +29,7 @@ class SingleLayerPerceptron(Perceptron):
         self.weightsHistory = [weights]
         self.weightsPerEpoch = []
         self.constants = Constants.getInstance()
+        self.isMultiLayer = isMultiLayer
 
     def train(self, inputs, expectedOutputs):
         self.activationFunction.configureNormalization(expectedOutputs)
@@ -60,7 +61,8 @@ class SingleLayerPerceptron(Perceptron):
         return error / len(expectedOutputs)
 
     def test(self, input):
-        return self.activationFunction(input, self.weights)
+        x = self.activationFunction(input, self.weights)
+        return x if self.isMultiLayer else self.activationFunction.denormalize(x)
 
     def incrementDeltaW(self, deltaW):
         self.deltaW = np.add(self.deltaW, deltaW)
@@ -78,7 +80,7 @@ class MultiLayerPerceptron(Perceptron):
         for qty, activationFn, weights in architecture:
             layer = []
             for i in range(qty):
-                n = SingleLayerPerceptron(activationFn, optimizerClass(optimizerOptions), weights[i])
+                n = SingleLayerPerceptron(activationFn, optimizerClass(optimizerOptions), weights[i], True)
                 layer.append(n)
             self.layers.append(layer)
         self.constants = Constants.getInstance()