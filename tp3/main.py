b113055cd5a2d65be763284c66f4b3e474e92d00
Alejo Flores Lucey <alejo@misterflores.com>
2024-10-05 19:31:25 -0300

    Add logic and initialization for digit recognizer
    Co-authored-by: Andres Carro Wetzel <acarro@itba.edu.ar>
    Co-authored-by: Mati Della Torre <mdella@itba.edu.ar>
    Co-authored-by: Ian Franco Tognetti <itognetti@itba.edu.ar>

--- tp3/main.py
+++ tp3/main.py
@@ -3,6 +3,7 @@ import json
 import os
 import pandas as pd
 import numpy as np
+from scipy.ndimage import gaussian_filter
 from src.constants import Constants
 from src.function import ActivationFunction
 from src.optimizer import OptimizerFunction
@@ -87,7 +88,7 @@ def solveSet(config):
 
     #TODO: decide if we want to show metrics
 
-# Exercise 3
+# Exercise 3 - Xor
 
 def solveMultilayerXor(config):
     dataset = pd.read_csv(XOR_DATASET_PATH)
@@ -121,6 +122,71 @@ def solveMultilayerXor(config):
 
     metrics = Metrics(expectedOutputs, predicted)
     metrics.displayAll()
+    
+# Exercise 3 - Digits
+
+DIGITS_REPRESENTATION_PATH = os.path.join(os.path.dirname(sys.argv[0]), "resources", "ej3", "digits.txt")
+DIGITS_DATASET_PATH = os.path.join(os.path.dirname(sys.argv[0]), "resources", "ej3", "digits.csv")
+def solveDigits(config):
+    representation = []
+    with open(DIGITS_REPRESENTATION_PATH, 'r') as file:
+        representation = file.readlines()
+    dataset = pd.read_csv(DIGITS_DATASET_PATH)
+    expectedOutputs = []
+    for o in dataset["y"].to_numpy():
+        outputRow = []
+        for i in range(10):
+            outputRow.append(1 if i == o else 0)
+        expectedOutputs.append(outputRow)
+
+    digits = []
+    currentInput = np.zeros((7,5))
+    for i in range(len(representation)):
+        if i != 0 and i % 7 == 0:
+            digits.append(currentInput)
+            currentInput = np.zeros((7,5))
+        row = representation[i][:-2].split(" ")
+        for j in range(len(row)):
+            currentInput[i % 7][j] = 1 if row[j] == "1" else 0
+    digits.append(currentInput)
+    
+    blurred = []
+    for digit in digits:
+        blurred.append(gaussian_filter(digit, sigma=0.4))
+    
+    inputs = []
+    for digit in digits:
+        newDigit = np.array([BIAS])
+        inputs.append(np.append(newDigit, digit.flatten()))
+    
+    architecture = []
+    for i, layer in enumerate(config["perceptron"]["architecture"]):
+        neuronQty = layer["neuronQty"]
+        f = ActivationFunction.getFunction(layer["activationFunction"]["type"], layer["activationFunction"]["options"])
+        weights = []
+        for _ in range(neuronQty):
+            weightsQty = config["perceptron"]["architecture"][i-1]["neuronQty"]+1 if i > 0 else len(inputs[0])
+            weights.append(Utils.initializeWeights(weightsQty))
+        architecture.append((neuronQty, f, weights))
+
+    o = OptimizerFunction.getFunction(config["learning"]["optimizer"]["type"])
+    p = MultiLayerPerceptron(architecture, o, config["learning"]["optimizer"]["options"])
+    p.train(inputs, expectedOutputs)
+
+    tests = []
+    for digit in blurred:
+        newDigit = np.array([BIAS])
+        tests.append(np.append(newDigit, digit.flatten()))
+    predicted = []
+    for digit in tests:
+        x = p.test(digit)
+        print(x)
+        predicted.append(np.argmax(x))
+
+    metrics = Metrics([0,1,2,3,4,5,6,7,8,9], predicted)
+    metrics.displayAll()
+
+    # Utils.mseVsEpoch(p, inputs, expectedOutputs, inputs, expectedOutputs, "digitsMSE.csv")
 
 
 if __name__ == "__main__":
@@ -152,6 +218,6 @@ if __name__ == "__main__":
             case "PARITY":
                 pass
             case "DIGITS":
-                pass
+                solveDigits(config)
 
     sys.exit(0)
