Fix normalization, definitely

tp3/src/function.py
@@ -81,10 +81,10 @@ class LogisticFunction(ActivationFunction):
         return 2 * beta * self.__call__(x, w) * (1 - self.__call__(x, w))
     
     def normalize(self, x):
-        return ((x-self.min)/(self.max-self.min))*(1-0)+0
+        return np.interp(x, [self.min, self.max], [0, 1])
     
     def denormalize(self, x):
-        return (((x-0)*(self.max-self.min))/(1-0))+self.min
+        return np.interp(x, [0, 1], [self.min, self.max])
 
 
 class HyperbolicTangentFunction(ActivationFunction):
@@ -97,7 +97,7 @@ class HyperbolicTangentFunction(ActivationFunction):
         return beta * (1 - np.power(self.__call__(x, w), 2))
     
     def normalize(self, x):
-        return ((x-self.min)/(self.max-self.min))*(1-(-1))-1
+        return np.interp(x, [self.min, self.max], [-1, 1])
     
     def denormalize(self, x):
-        return (((x-(-1))*(self.max-self.min))/(1-(-1)))+self.min
+        return np.interp(x, [-1, 1], [self.min, self.max])

tp3/src/perceptron.py
@@ -38,9 +38,7 @@ class SingleLayerPerceptron(Perceptron):
                 seenInputs += 1
 
                 output = self.activationFunction(input, self.weights)
-                deltaW = self.optimizer((self.activationFunction.normalize(expectedOutput) - output) * self.activationFunction.derivative(input, self.weights) * input)
-                # FIXME: How to normalize/denormalize? Waiting for response
-                # deltaW = self.optimizer((expectedOutput - self.activationFunction.denormalize(output)) * self.activationFunction.derivative(input, self.weights) * input)
+                deltaW = self.optimizer((expectedOutput - self.activationFunction.denormalize(output)) * self.activationFunction.derivative(input, self.weights) * input)
                 self.incrementDeltaW(deltaW)
 
                 if self.constants.updateWeightsEveryXInputs != -1 and seenInputs % self.constants.updateWeightsEveryXInputs == 0:
@@ -58,9 +56,7 @@ class SingleLayerPerceptron(Perceptron):
     def calculateError(self, inputs, expectedOutputs):
         error = 0
         for input, expectedOutput in zip(inputs, expectedOutputs):
-            error += np.power(self.activationFunction.normalize(expectedOutput) - self.activationFunction(input, self.weights), 2)
-            # FIXME: How to normalize/denormalize? Waiting for response
-            # error += np.power(expectedOutput - self.activationFunction.denormalize(self.activationFunction(input, self.weights)), 2)
+            error += np.power(expectedOutput - self.activationFunction.denormalize(self.activationFunction(input, self.weights)), 2)
         return error / len(expectedOutputs)
 
     def test(self, input):