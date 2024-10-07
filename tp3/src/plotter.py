032e8ad622d6268b86b2b5470c99c75428b8c9e1
Matías Della Torre <matidellatorre@gmail.com>
2024-10-06 22:08:18 -0300

    feat: Ex1 plotter

--- tp3/src/plotter.py
+++ tp3/src/plotter.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib.animation import FuncAnimation
 
 
 class Plotter:
@@ -19,3 +21,46 @@ class Plotter:
         plt.ylabel("Mean Squared Error")
         plt.legend()
         plt.show()
+
+
+    def graphEx1(dataset, weightsPerEpoch):
+        fig, ax = plt.subplots()
+
+        ax.set_xticks([-1, 1])
+        ax.set_yticks([-1, 1])
+
+        ax.set_xlim(-1.5, 1.5)
+        ax.set_ylim(-1.5, 1.5)
+
+        ax.spines['left'].set_position('center')
+        ax.spines['bottom'].set_position('center')
+
+        ax.spines['right'].set_color('none')
+        ax.spines['top'].set_color('none')
+
+        ax.xaxis.set_ticks_position('bottom')
+        ax.yaxis.set_ticks_position('left')
+
+        scatter = ax.scatter(dataset["x1"], dataset["x2"])
+        line, = ax.plot([], [], lw=2, color='black')
+
+        def init():
+            line.set_data([], [])
+            return line,
+
+        def update(epoch):
+            ax.set_title(f'Separation Hyperplane (epoch {epoch+1})')
+            x_values = np.array([-1.5, 1.5])
+            m = -weightsPerEpoch[epoch][1] / weightsPerEpoch[epoch][2]
+            b = -weightsPerEpoch[epoch][0] / weightsPerEpoch[epoch][2]
+            y_values = m * x_values + b
+            line.set_data(x_values, y_values)
+            side = dataset["x2"] - (m * dataset["x1"] + b)
+            colors = np.where(side >= 0, 'blue', 'red')
+            scatter.set_color(colors)
+            return line,
+
+        ani = FuncAnimation(fig, update, frames=len(weightsPerEpoch), init_func=init, blit=True)
+
+        ani.save('perceptron_animation.gif', writer='imagemagick')
+        
