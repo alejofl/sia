adb22c3f94a34f09be1f69e02ed2b2304dd19ce6
Andres Carro <acarro@itba.edu.ar>
2024-10-18 18:09:24 -0300

    Add standarization of dataset
    Co-authored-by: alu abu <afloreslucey@itba.edu.ar>

--- tp4/src/utils.py
+++ tp4/src/utils.py
@@ -8,8 +8,18 @@ class Utils:
     EUROPE_DATASET_PATH = os.path.join(os.path.dirname(sys.argv[0]), "resources", "europe.csv")
 
     @staticmethod
-    def getEuropeDataset():        
+    def getEuropeDataset():
         df = pd.read_csv(Utils.EUROPE_DATASET_PATH)
+        df = Utils.zScoreData(df)
         countries = df["Country"].to_numpy()
         data = df.reset_index().drop(columns=["Country"]).to_numpy()
         return countries, data
+
+    @staticmethod
+    def zScoreData(df):  # TODO: Check
+        def zScore(col):
+            mean = col.mean()
+            std = col.std()
+            return col.apply(lambda x: (x - mean) / std)
+
+        return df.apply(lambda col: zScore(col) if col.name != "Country" else col)
