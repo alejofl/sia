ef0c50634eea92f75e5f3f7362c6d0bf6df8b838
Alejo Flores Lucey <alejo@misterflores.com>
2024-11-23 01:27:38 -0300

    Continue with plots
    Co-authored-by: Andres Carro Wetzel <acarro@itba.edu.ar>

--- tp5/plot.py
+++ tp5/plot.py
@@ -1,18 +1,22 @@
 import pickle
+from resources.font import font3 as font
+from src.utils import Utils
 from src.plotter import Plotter
 
 
 if __name__ == "__main__":
+    Plotter.drawLetters(Utils.parseFont(font))
+    
     # Plotter.mseVsEpoch("mseVsEpoch_cae_04_24letters.csv")
     
     # Plotter.latentSpace("latentSpaceOutput_cae_04_24letters.csv")
     
-    with open("lettersAE_cae_04_24letters.pickle", 'rb') as file:
-        ae = pickle.load(file)
-        Plotter.newElements(
-            ae,
-            8,
-            [-1, 1],
-            [-1, 1],
-            (7, 5)
-        )
+    # with open("lettersAE_cae_04_24letters.pickle", 'rb') as file:
+    #     ae = pickle.load(file)
+    #     Plotter.newElements(
+    #         ae,
+    #         8,
+    #         [-1, 1],
+    #         [-1, 1],
+    #         (7, 5)
+    #     )
