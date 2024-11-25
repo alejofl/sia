1f0e928a38881aff30fbede7d9c4ec3e73810714
Alejo Flores Lucey <alejo@misterflores.com>
2024-11-25 16:58:29 -0300

    Add missing conventional autoencoder study

--- tp5/plot.py
+++ tp5/plot.py
@@ -9,9 +9,9 @@ if __name__ == "__main__":
     
     # Plotter.mseVsEpoch("mseVsEpoch_cae_04_24letters.csv")
     
-    # Plotter.latentSpace("latentSpaceOutput_cae_04_24letters.csv")
+    # Plotter.latentSpace("latentSpaceOutput_cae_04_24letters_batch.csv")
     
-    # with open("lettersAE_cae_04_24letters.pickle", 'rb') as file:
+    # with open("lettersAE_cae_04_24letters_batch.pickle", 'rb') as file:
     #     ae = pickle.load(file)
     #     Plotter.newElements(
     #         ae,
@@ -21,14 +21,4 @@ if __name__ == "__main__":
     #         (7, 5)
     #     )
     
-    # Plotter.drawIcons(Utils.parseIcons("icons", 8))
-    # Plotter.latentSpace("latentSpaceOutput_vae.csv")
-    with open("iconsVAE.pickle", 'rb') as file:
-        ae = pickle.load(file)
-        Plotter.newElements(
-            ae,
-            8,
-            [-3, 3],
-            [-3, 3],
-            (20, 20)
-        )
+    Plotter.drawIcons(Utils.parseIcons("icons", 8))
